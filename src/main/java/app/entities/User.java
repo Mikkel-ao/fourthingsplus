f2f3a195e37ac2ce76b75f941c6883837326d89e
Mikkel-ao <mikkel.ao.dev@gmail.com>
2025-03-15 12:03:01 +0100

    Adding local project to repo

---
+++ src/main/java/app/entities/User.java
@@ -0,0 +1,41 @@
+package app.entities;
+
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+    private String role;
+
+    public User(int userId, String username, String password, String role) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
